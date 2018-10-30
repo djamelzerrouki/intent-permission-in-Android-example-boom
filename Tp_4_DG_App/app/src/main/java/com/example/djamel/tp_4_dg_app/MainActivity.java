30dfe13f9df86ea39b272c93013a8601d5d26a5e
zerrouki djamel <djameljimmizerrouki@gmail.com>
2018-10-30 08:46:07 +0100

    commit 3

--- Tp_4_DG_App/app/src/main/java/com/example/djamel/tp_4_dg_app/MainActivity.java
+++ Tp_4_DG_App/app/src/main/java/com/example/djamel/tp_4_dg_app/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.djamel.tp_4_dg_app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toast.makeText(this,"hi jimmi",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"BOOM :o ",Toast.LENGTH_LONG).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void my_brawser(View view) {
+
+        Uri url = Uri.parse("http://www.google.com");
+        Intent intentMybrwser = new Intent(Intent.ACTION_VIEW, url);
+        startActivity(intentMybrwser);
+        Toast.makeText(this,"My browser ",Toast.LENGTH_LONG).show();
+
     }
 }
